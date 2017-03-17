c1eb1b06a3df24c2cb420ad5c813979265aa5185
Armienn <nigathr@gmail.com>
2017-03-17 16:43:59 +0100

    Improved drawing stuff

--- draw/script.js
+++ draw/script.js
@@ -2,7 +2,8 @@ function getEl(id){
 	return document.getElementById(id)
 }
 
-//getEl("output").innerHTML = "Jürgen Rattenfänger"
+var lineSize = 3
+var lineColor = "yellow"
 
 var canvas = getEl("canvas")
 var context = canvas.getContext("2d")
@@ -22,18 +23,51 @@ function draw(){
 }
 
 var mouseDown = false
-window.onmousedown = function(){
+window.onmousedown = function(event){
 	mouseDown = true
+	context.beginPath()
+	context.moveTo(event.x, event.y)
+	context.lineTo(event.x, event.y)
+	context.lineWidth = lineSize
+	context.strokeStyle = lineColor
+	context.lineCap = "round"
+	context.stroke()
+	//context.fillStyle = lineColor
+    //context.fillRect(Math.floor(event.x-lineSize/2), Math.floor(event.y-lineSize/2), lineSize, lineSize)
+    lastX = event.x
+    lastY = event.y
 }
-window.onmouseup = function(){
+window.onmouseup = function(event){
 	mouseDown = false
+    lastX = -1
+    lastY = -1
 }
 
+var lastX = -1
+var lastY = -1
 window.onmousemove = function(event){
-	context.fillStyle = "pink"
-	if(mouseDown)
-		context.fillRect(event.x, event.y, 3, 3)
+	if(mouseDown){
+        //context.fillStyle = lineColor
+		//context.fillRect(Math.floor(event.x-lineSize/2), Math.floor(event.y-lineSize/2), lineSize, lineSize)
+        if(lastX >= 0 && lastY >= 0){
+            context.beginPath()
+            context.moveTo(lastX, lastY)
+            context.lineTo(event.x, event.y)
+            context.lineWidth = lineSize
+            context.strokeStyle = lineColor
+			context.lineCap = "round"
+            context.stroke()
+        }
+        lastX = event.x
+        lastY = event.y
+    }
 }
 
+window.onmousewheel = (event)=>{
+    if(event.deltaY > 0)
+        lineSize--
+    else
+        lineSize++
+}
 
 draw()
