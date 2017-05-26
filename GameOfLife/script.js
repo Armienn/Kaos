f16a8d876bb8846ef017fe51e00a2a001713bbf4
Armienn <nigathr@gmail.com>
2017-05-26 11:35:02 +0200

    Extended game

--- GameOfLife/script.js
+++ GameOfLife/script.js
@@ -73,10 +73,13 @@ window.onmousewheel = (event)=>{
 //draw()
 
 class Game {
-    constructor(width, height, wrapWorld = true, aliveRatio = 0){
+    constructor(width, height, borderMode = "wrap", aliveRatio = 0){
+        this.spawnPopulations = [3]
+        this.survivalPopulations = [2,3]
+        this.factions = 3
         this.width = width
         this.height = height
-        this.wrapWorld = wrapWorld
+        this.borderMode = borderMode
         this.setupGrid(aliveRatio)
     }
 
@@ -87,7 +90,7 @@ class Game {
             for(var j = 0; j < this.height; j++){
                 this.grid[i][j] = 0
                 if(aliveRatio && aliveRatio > Math.random())
-                    this.grid[i][j] = 1
+                    this.grid[i][j] = 1 + Math.floor(Math.random()*this.factions)
             }
         }
     }
@@ -103,24 +106,43 @@ class Game {
     }
 
     newStateFor(x, y){
-        var count = this.countAdjacent(x,y)
+        var counts = this.countAdjacent(x,y)
+        var count = 0
+        var majorities = [0]
+        var max = 0
+        for(var i=0; i<this.factions; i++){
+            count += counts[i]
+            if(counts[i]>max){
+                max = counts[i]
+                majorities = [i]
+            }
+            else if(counts[i]==max){
+                majorities.push(i)
+            }
+        }
         if(this.grid[x][y]){
-            if(count == 2 || count == 3)
-                return 1
+            if(this.survivalPopulations.indexOf(count) !== -1)
+                return this.selectRandom(majorities)+1
         }
         else
-            if(count == 3)
-                return 1
+            if(this.spawnPopulations.indexOf(count) !== -1)
+                return this.selectRandom(majorities)+1
         return 0
     }
 
+    selectRandom(list){
+        return list[Math.floor(Math.random()*list.length)]
+    }
+
     countAdjacent(x, y){
-        var count = 0
+        var counts = []
+        for(var i=0; i<this.factions; i++)
+            counts[i] = 0
         this.forAdjacent(x,y, (state)=>{
             if(state)
-                count++
+                counts[state-1]++
         })
-        return count
+        return counts
     }
 
     forAdjacent(x, y, callback){
@@ -129,11 +151,13 @@ class Game {
                 if(i==x && j==y)
                     continue
                 if(i < 0 || j < 0 || this.width <= i || this.height <= j){
-                    if(this.wrapWorld){
+                    if(this.borderMode == "wrap"){
                         var wrapI = i<0 ? this.width-1 : (this.width <= i ? 0 : i)
                         var wrapJ = j<0 ? this.height-1 : (this.height <= j ? 0 : j)
                         callback(this.grid[wrapI][wrapJ])
                     }
+                    else if(this.borderMode > 0)
+                        callback(this.borderMode)
                     else
                         callback(0)
                 }
@@ -165,16 +189,54 @@ class GameDisplay {
     }
 
     drawGrid(){
+        var colours = ["white"]
+        for(var i=0; i<this.game.factions; i++){
+            var rgb = GameDisplay.hsvToRgb(GameDisplay.integerToZeroToOneMap(i), 1, 0.8)
+            colours[i+1] = "rgb("+Math.floor(rgb[0])+","+Math.floor(rgb[1])+","+Math.floor(rgb[2])+")"
+        }
+        if(this.game.factions == 1)
+            colours[1] = "black"
         for(var i = 0; i < this.game.width; i++){
             for(var j = 0; j < this.game.height; j++){
-                context.fillStyle = this.game.grid[i][j] ? "black" : "white"
+                context.fillStyle = colours[this.game.grid[i][j]]
             	context.fillRect(i*this.cellSize,j*this.cellSize,this.cellSize,this.cellSize)
             }
         }
     }
+
+    static integerToZeroToOneMap(n){
+        if(n<1) return 0
+        var m = GameDisplay.weirdRoundToPowerOfTwo(n)
+        return (1+2*(n - m/2))/m
+    }
+
+    static weirdRoundToPowerOfTwo(n){
+        return Math.pow(2,Math.floor(Math.log2(n*2)))
+    }
+
+    static hsvToRgb(h, s, v) {
+        var r, g, b;
+
+        var i = Math.floor(h * 6);
+        var f = h * 6 - i;
+        var p = v * (1 - s);
+        var q = v * (1 - f * s);
+        var t = v * (1 - (1 - f) * s);
+
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+        }
+
+        return [ r * 255, g * 255, b * 255 ];
+        }
 }
 
-var game = new Game(50, 50, true, 0.2)
+var game = new Game(50, 50, "wrap", 0.2)
 var display = new GameDisplay(game)
 
 game.run(100, ()=>{display.drawGrid()})
